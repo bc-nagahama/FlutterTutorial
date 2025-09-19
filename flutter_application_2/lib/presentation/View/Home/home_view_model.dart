c6c0e8c364620416bf3c8985414e145e8602d390
bc-nagahama <yuka.nagahama@mail.benesse.co.jp>
2025-09-19 10:33:31 +0900

    【修正】不要な記述を削除

--- flutter_application_2/lib/presentation/View/Home/home_view_model.dart
+++ flutter_application_2/lib/presentation/View/Home/home_view_model.dart
@@ -1,10 +1,6 @@
-import 'dart:developer';
 import 'package:flutter_application_2/data/providers/subject_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_application_2/data/datasources/subject_datasource.dart';
 import 'home_state.dart';
-import 'package:flutter_application_2/data/repositories/subject_repository_impl.dart';
 import 'package:flutter_application_2/domain/repositories/subject_repository.dart';
 
 final homeProvider = StateNotifierProvider<HomeViewModel, HomeState>(
@@ -21,27 +17,37 @@ class HomeViewModel extends StateNotifier<HomeState>{
   }
 
   final SubjectRepository subjectRepository;
-  
+
+
+  ///
+  ///初期化処理
+  ///
+  Future<void> initialize() async{
+    //抽象を呼び出していて具体（SubjectRepositoryImpl）は知らない
+    final subjectsEntity = await subjectRepository.getSubjects(); 
+
+    state = state.copyWith(
+      subjectName1: subjectsEntity.subjects[0].subjectName,
+      subjectName2: subjectsEntity.subjects[1].subjectName,
+      unitName2: subjectsEntity.subjects[1].unit.unitName,
+      grade1: subjectsEntity.subjects[0].unit.grade,
+      unitName1: subjectsEntity.subjects[0].unit.unitName,
+      grade2: subjectsEntity.subjects[1].unit.grade,
+      lessonName1: subjectsEntity.subjects[0].unit.lesson.lessonName,
+      lessonName2: subjectsEntity.subjects[1].unit.lesson.lessonName,
+      difficulty1: subjectsEntity.subjects[0].unit.lesson.difficulty,
+      difficulty2: subjectsEntity.subjects[1].unit.lesson.difficulty,
+    );
+  }
+
+
+  ///
+  ///XPの増加処理
+  ///
   Future<void> incrementCounter() async{
     state = state.copyWith(counter: state.counter + 1);
   }
   
-  Future<void> initialize() async{
-    final subjectList = await subjectRepository.getSubjects();
-
-    state = state.copyWith(
-      subjectName1: subjectList.subjects[0].subjectName,
-      subjectName2: subjectList.subjects[1].subjectName,
-      unitName1: subjectList.subjects[0].unit.unitName,
-      unitName2: subjectList.subjects[1].unit.unitName,
-      grade1: subjectList.subjects[0].unit.grade,
-      grade2: subjectList.subjects[1].unit.grade,
-      lessonName1: subjectList.subjects[0].unit.lesson.lessonName,
-      lessonName2: subjectList.subjects[1].unit.lesson.lessonName,
-      difficulty1: subjectList.subjects[0].unit.lesson.difficulty,
-      difficulty2: subjectList.subjects[1].unit.lesson.difficulty,
-    );
-  }
 }
 
 
