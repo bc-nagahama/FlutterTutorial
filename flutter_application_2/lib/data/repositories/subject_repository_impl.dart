25c643716f642c4edf32039181ef61154c57115a
bc-nagahama <yuka.nagahama@mail.benesse.co.jp>
2025-09-19 10:32:49 +0900

    【修正】@Queries()を使わないため不要な記述を削除

--- flutter_application_2/lib/data/repositories/subject_repository_impl.dart
+++ flutter_application_2/lib/data/repositories/subject_repository_impl.dart
@@ -4,16 +4,14 @@ import 'package:flutter_application_2/domain/entities/subjects.dart';
 import 'package:flutter_application_2/domain/repositories/subject_repository.dart';
 
 class SubjectRepositoryImpl implements SubjectRepository{
-  //SubjectDatasourceを受け取ってAPIを叩くときに利用する
   SubjectRepositoryImpl(this.subjectDatasource);
 
   final SubjectDatasource subjectDatasource;
 
   @override
   Future<Subjects> getSubjects() async{
-    final request = SubjectsResponse();
 
-    final response = await subjectDatasource.getSubjects(request);
+    final response = await subjectDatasource.getSubjects();
 
     return response.toEntity();
   }
