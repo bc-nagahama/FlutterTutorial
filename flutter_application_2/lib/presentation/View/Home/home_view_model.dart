488c1506211f13d13b3aadfcc857899576d71d62
bc-nagahama <yuka.nagahama@mail.benesse.co.jp>
2025-09-25 11:43:27 +0900

    【修正】HomeStateに合わせて修正

--- flutter_application_2/lib/presentation/View/Home/home_view_model.dart
+++ flutter_application_2/lib/presentation/View/Home/home_view_model.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_application_2/data/providers/subject_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'home_state.dart';
+import 'package:flutter_application_2/presentation/view/home/home_state.dart';
 import 'package:flutter_application_2/domain/repositories/subject_repository.dart';
 import 'package:flutter_application_2/data/repositories/mock/subject_repository_mock.dart';
 
@@ -37,16 +37,8 @@ class HomeViewModel extends StateNotifier<HomeState>{
     final subjectsEntity = await subjectRepository.getSubjects(); 
 
     state = state.copyWith(
-      subjectName1: subjectsEntity.subjects[0].subjectName,
-      subjectName2: subjectsEntity.subjects[1].subjectName,
-      unitName2: subjectsEntity.subjects[1].unit.unitName,
-      grade1: subjectsEntity.subjects[0].unit.grade,
-      unitName1: subjectsEntity.subjects[0].unit.unitName,
-      grade2: subjectsEntity.subjects[1].unit.grade,
-      lessonName1: subjectsEntity.subjects[0].unit.lesson.lessonName,
-      lessonName2: subjectsEntity.subjects[1].unit.lesson.lessonName,
-      difficulty1: subjectsEntity.subjects[0].unit.lesson.difficulty,
-      difficulty2: subjectsEntity.subjects[1].unit.lesson.difficulty,
+      subject1: subjectsEntity.subjects[0],
+      subject2: subjectsEntity.subjects[1],
     );
   }
 
